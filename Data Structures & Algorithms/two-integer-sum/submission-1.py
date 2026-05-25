ab5144ba478a29b7a7ec4455c738a68dd6a517f5
kunal <96532810+Kunal152000@users.noreply.github.com>
2026-05-25 14:57:54 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for index, val in enumerate(nums):
+            difference = target - val
+            if difference in seen:
+                return [seen[difference],index]
+            else:
+                seen[val] = index
+        
